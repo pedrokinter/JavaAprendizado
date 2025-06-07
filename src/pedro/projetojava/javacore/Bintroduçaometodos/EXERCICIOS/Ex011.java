9553ffb25591f7941e40aac725bd2e4940ce35b2
Pedro Henrique <pedrokinter1@gmail.com>
2025-06-07 11:54:11 -0300

    zzzz

--- src/pedro/projetojava/javacore/Bintroduçaometodos/EXERCICIOS/Ex011.java
+++ src/pedro/projetojava/javacore/Bintroduçaometodos/EXERCICIOS/Ex011.java
@@ -1,9 +1,32 @@
 package pedro.projetojava.javacore.Bintroduçaometodos.EXERCICIOS;
 
+import pedro.projetojava.javacore.Bintroduçaometodos.dominioExercicios.Retangulo011;
+
 import java.util.Scanner;
 
 public class Ex011 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        Retangulo011 retangulo1 = new Retangulo011();
+        Retangulo011 retangulo2 = new Retangulo011();
+        Retangulo011 imprimir = new Retangulo011();
+        retangulo1.altura = entrada.nextDouble();
+        retangulo1.largura = entrada.nextDouble();
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=");
+
+        retangulo2.altura = entrada.nextDouble();
+        retangulo2.largura = entrada.nextDouble();
+
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=");
+
+
+        System.out.println(retangulo1.area(retangulo1));
+        System.out.println(retangulo2.area(retangulo2));
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=-");
+        System.out.println("O perimetro do primeiro retângulo é igual a: " +retangulo1.perimetro(retangulo1));
+        System.out.println("O perimetro do  segundo retângulo é igual a: " +retangulo2.perimetro(retangulo2));
+        System.out.println("--------------------------");
+        System.out.println(retangulo1.comparar(retangulo1, retangulo2));
+
     }
 }
--- src/pedro/projetojava/javacore/Bintroduçaometodos/dominioExercicios/Retangulo011.java
+++ src/pedro/projetojava/javacore/Bintroduçaometodos/dominioExercicios/Retangulo011.java
@@ -3,17 +3,38 @@ package pedro.projetojava.javacore.Bintroduçaometodos.dominioExercicios;
 public class Retangulo011 {
     public double largura;
     public double altura;
+    public double area;
 
-    public double area() {
+    public double area(Retangulo011 retangulo) {
+       return area = altura * largura;
+    }
+
+    public double perimetro(Retangulo011 retangulo) {
+        return 2 * (altura + largura);
 
     }
 
-    public double perimetro() {
-
+    public boolean comparar(Retangulo011 retangulo) {
+        if (this.area > retangulo.area) {
+            System.out.println("O primeiro retângulo possui a maior área");
+            return true;
+        } else {
+            System.out.println("O segundo retângulo possui a maior área");
+            return false;
+        }
 
     }
 
-    public double compararArea() {
-
+    public void teste(Retangulo011 retangulo) {
+        if (this.area > retangulo.area) {
+            System.out.println("area 1 > area 2");
+        } else {
+            System.out.println("area 1 < area 2");
+        }
     }
+
+
+
+
+
 }
