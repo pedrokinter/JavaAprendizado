d925f1887fc423cf156ef8e512529066536e7760
Pedro Henrique <pedrokinter1@gmail.com>
2025-06-23 19:54:56 -0300

    estudoszinhussssssss

--- src/pedro/projetojava/javacore/Dconstrutores/dominioExercicios/cursoCadastro.java
+++ src/pedro/projetojava/javacore/Dconstrutores/dominioExercicios/cursoCadastro.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class cursoCadastro {
     private String nomeCurso;
     private String [] alunos = new String[5];
+    private String [] matriculas = new String[5];
     private int indice;
     alunoCadastro aluno = new alunoCadastro();
 
@@ -19,18 +20,24 @@ public class cursoCadastro {
 
     public void matricularAluno (alunoCadastro aluno){
         this.alunos[this.indice] = aluno.getNome();
+        this.matriculas[this.indice] = aluno.getMatricula();
         this.indice += 1;
     }
 
     public void matricularAluno (String nome, String matricula){
 
+        aluno.setNome(nome);
+        aluno.setMatricula(matricula);
         matricularAluno(aluno);
     }
 
 
     public void exibirAlunos() {
-        System.out.println("Nome do curso: " + this.nomeCurso);
+        System.out.println("=-=-=-=-=-=-=-=");
+        System.out.println("Nome do curso: " + this.nomeCurso.toUpperCase());
         System.out.println("Alunos cadastrados: " + Arrays.toString(alunos));
+        System.out.println("Matriculas do curso: " + Arrays.toString(matriculas));
+        System.out.println("=-=-=-=-=-=-=-=");
     }
 
 
@@ -57,6 +64,10 @@ public class cursoCadastro {
     }
 
     public void setIndice(int indice) {
+        if (indice > 5) {
+            System.out.println("erro, indice maior q 5 ");
+            return;
+        }
         this.indice = indice;
     }
 
