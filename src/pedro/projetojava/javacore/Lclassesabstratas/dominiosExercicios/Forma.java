9ae5ee060420e1f3aa4a3ad0d6bb1cf372306fc8
Pedro Henrique <pedrokinter1@gmail.com>
2025-07-30 14:43:04 -0300

    Exercicioszinhussssssssssssssss

---
+++ src/pedro/projetojava/javacore/Lclassesabstratas/dominiosExercicios/Forma.java
@@ -0,0 +1,10 @@
+package pedro.projetojava.javacore.Lclassesabstratas.dominiosExercicios;
+
+public abstract class Forma {
+
+    public abstract void calcularArea();
+
+    public abstract void calcularPerimetro();
+
+
+}
